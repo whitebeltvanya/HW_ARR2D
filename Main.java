3b6f62f8b458cbf273b0ab1668c0372aa3ce524d
vanya <whitebeltvanya@gmail.com>
2025-07-07 14:50:28 +0300

    added color depth constant

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Main {
 
     public static final int SIZE = 8;
+    public static final int COLOR_DEPTH = 256;
 
     public static void main(String[] args) {
 
@@ -34,7 +35,7 @@ public class Main {
         Random random = new Random();
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                matrix[i][j] = random.nextInt(256);
+                matrix[i][j] = random.nextInt(COLOR_DEPTH);
             }
         }
     }
